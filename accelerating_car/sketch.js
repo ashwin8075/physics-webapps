f7d48ecc768a3216dd1a9ff9b771a741210620ed
limpengus <notreal@email.com>
2023-08-26 12:56:51 -0700

    formatting

--- accelerating_car/sketch.js
+++ accelerating_car/sketch.js
@@ -8,16 +8,16 @@ let dt = 1;
 
 function setup() {
   createCanvas(600, 600);
-  back_wheel_center = createVector(0,469);
-  front_wheel_center = createVector(100,469);
-  chassis_origin = createVector(15,400);
+  back_wheel_center = createVector(0, 469);
+  front_wheel_center = createVector(100, 469);
+  chassis_origin = createVector(15, 400);
 }
 
 function draw() {
   background(0);
-back_wheel_center.x = u * dt + (acc*dt*dt)/2 + back_wheel_center.x; // this is for back wheel
- front_wheel_center.x = u * dt + (acc*dt*dt)/2 + front_wheel_center.x; // this is for front wheel 
- chassis_origin.x = u * dt + (acc*dt*dt)/2 + chassis_origin.x; // this is for top chasis 
+  back_wheel_center.x = u * dt + (acc * dt * dt) / 2 + back_wheel_center.x; // this is for back wheel
+  front_wheel_center.x = u * dt + (acc * dt * dt) / 2 + front_wheel_center.x; // this is for front wheel 
+  chassis_origin.x = u * dt + (acc * dt * dt) / 2 + chassis_origin.x; // this is for top chasis 
 
   dt = dt + 0.1;
 
@@ -25,7 +25,7 @@ back_wheel_center.x = u * dt + (acc*dt*dt)/2 + back_wheel_center.x; // this is f
   strokeWeight(8);
   fill(0);
   circle(back_wheel_center.x, back_wheel_center.y, 50);
-  circle(front_wheel_center.x,front_wheel_center.y, 50);
-  rect(5,495,590,100);
-  rect(chassis_origin.x,chassis_origin.y,70,45);
+  circle(front_wheel_center.x, front_wheel_center.y, 50);
+  rect(5, 495, 590, 100);
+  rect(chassis_origin.x, chassis_origin.y, 70, 45);
 }
