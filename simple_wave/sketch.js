3db76475b85b9b2214237f6daeac1d90b70a08a1
limpengus <notreal@email.com>
2023-10-05 22:36:04 -0700

    added simple_wave app

---
+++ simple_wave/sketch.js
@@ -0,0 +1,57 @@
+// Wave program using p5.js
+// User can adjust frequency, wavelength, and amplitude
+
+// TODO:
+// - put names for sliders and show the values with units
+// - show rulers on x and y axes
+
+let wavelength;     // wavelength of wave
+let frequency;      // frequency of wave
+let amplitude;      // amplitude of wave
+let phase;          // phase angle of wave
+let time_now;           // time
+
+let sliderWavelength;   // wavelength slider
+let sliderFrequency;    // frequency slider
+let sliderAmplitude;    // amplitude slider
+
+function setup() {
+  createCanvas(600, 400);
+
+  // Set initial values
+  wavelength = 100;
+  frequency = 0;
+  amplitude = 50;
+  phase = PI;
+  time_now = 0;
+
+  // Create sliders for user input
+  sliderWavelength = createSlider(150, 600, wavelength, 10);
+  sliderWavelength.position(20, 20);
+  sliderFrequency = createSlider(0, 10, frequency, 0.1);
+  sliderFrequency.position(20, 50);
+  sliderAmplitude = createSlider(0, 100, amplitude, 1);
+  sliderAmplitude.position(20, 80);
+}
+
+function draw() {
+  background(220);
+
+  // Get user input from sliders
+  wavelength = sliderWavelength.value();
+  frequency = sliderFrequency.value();
+  amplitude = sliderAmplitude.value();
+
+  // Draw wave
+  noFill();
+  stroke(0);
+  beginShape();
+  for (let x = 0; x <= width; x += 10) {
+    let y = amplitude * sin(2 * PI * (x / wavelength - time_now * frequency) + phase);
+    vertex(x, height / 2 + y);
+  }
+  endShape();
+
+  // Update time
+  time_now += 0.01;
+}
