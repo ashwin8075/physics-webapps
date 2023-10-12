9878f582c38dee1af4f945adc1005dfcb31755f1
limpengus <notreal@email.com>
2023-10-12 10:53:41 -0700

    switched to standard units

--- simple_wave/sketch.js
+++ simple_wave/sketch.js
@@ -3,20 +3,10 @@
 
 // TODO:
 // - (d) put names for sliders and show the values with units
-// - (n) show rulers on x and y axes
-// - switch to an equation (and sliders) using wavelength, period
+// - (d) show rulers on x and y axes
+// - switch to an equation (and sliders) using wavelength, timeperiod
 // - show the wave equation: y = sin(2 pi x / lamda − 2 pi t / T + phi)
 // - also show v = lambda / T with real-time value for v
-
-let wavelength;     // wavelength of wave
-let frequency;      // frequency of wave
-let amplitude;      // amplitude of wave
-let phase;          // phase angle of wave
-let time_now;           // time
-
-let sliderWavelength;   // wavelength slider
-let sliderFrequency;    // frequency slider
-let sliderAmplitude;    // amplitude slider
 
 function setup() {
   createCanvas(windowWidth*0.95, windowHeight*0.95);
@@ -24,44 +14,45 @@ function setup() {
   // set simulation length units in terms of window dimensions
   sim_1m_x = windowWidth / 100;
   sim_1m_y = windowHeight / 100;
+  sim_time_step_secs = 1/60;
 
   // Set initial values
-  wavelength = 100;
-  frequency = 0;
-  amplitude = 50;
+  wavelength = 20; // m | [2, 100]
+  timeperiod = 2; // s | [0.1, 5]
+  amplitude = 5; // m | [0, 10]
   phase = PI;
   time_now = 0;
 
   // Create sliders for user input
   //
   // wavelength slider
-  sliderWavelength = createSlider(150, 600, wavelength, 10);
+  sliderWavelength = createSlider(2, 100, wavelength, 1);
   sliderWavelength.position(20, 20);
   wavelengthBox = createInput();
   wavelengthBox.position(180, 20);
   wavelengthBox.size(50);
   wavelengthBox.value(wavelength);
-  wavelengthName = createP('Wavelength');
+  wavelengthName = createP('Wavelength L (m)');
   wavelengthName.position(20, -10);
   //
-  // frequency slider
-  sliderFrequency = createSlider(0, 10, frequency, 0.1);
-  sliderFrequency.position(20, 50);
-  frequencyBox = createInput();
-  frequencyBox.position(180, 50);
-  frequencyBox.size(50);
-  frequencyBox.value(frequency);
-  frequencyName = createP('Frequency');
-  frequencyName.position(20, 20);
+  // timeperiod slider
+  sliderTimeperiod = createSlider(0.1, 5, timeperiod, 0.1);
+  sliderTimeperiod.position(20, 50);
+  timeperiodBox = createInput();
+  timeperiodBox.position(180, 50);
+  timeperiodBox.size(50);
+  timeperiodBox.value(timeperiod);
+  timeperiodName = createP('Time-period T (s)');
+  timeperiodName.position(20, 20);
   //
   // amplitude slider
-  sliderAmplitude = createSlider(0, 100, amplitude, 1);
+  sliderAmplitude = createSlider(0, 10, amplitude, 1);
   sliderAmplitude.position(20, 80);
   amplitudeBox = createInput();
   amplitudeBox.position(180, 80);
   amplitudeBox.size(50);
   amplitudeBox.value(amplitude);
-  amplitudeName = createP('Amplitude');
+  amplitudeName = createP('Amplitude A (m)');
   amplitudeName.position(20, 50);
 }
 
@@ -75,7 +66,7 @@ function draw() {
   strokeWeight(1);
   scale_left_x = 3*sim_1m_x
   scale_right_x = scale_left_x + 10*sim_1m_x
-  scale_y = windowHeight - 10*sim_1m_y
+  scale_y = height - 5*sim_1m_y
   line(scale_left_x, scale_y, scale_right_x, scale_y);
   line(scale_left_x, scale_y - 1*sim_1m_y, scale_left_x, scale_y + 1*sim_1m_y);
   line(scale_right_x, scale_y - 1*sim_1m_y, scale_right_x, scale_y + 1*sim_1m_y);
@@ -83,7 +74,7 @@ function draw() {
   //
   // draw y-axis scale bar
   scale_x = 3*sim_1m_x
-  scale_top_y = windowHeight - 25*sim_1m_y
+  scale_top_y = height - 20*sim_1m_y
   scale_bottom_y = scale_top_y + 10*sim_1m_y
   line(scale_x, scale_top_y, scale_x, scale_bottom_y);
   line(scale_x - 1*sim_1m_x, scale_top_y, scale_x + 1*sim_1m_x, scale_top_y);
@@ -93,23 +84,32 @@ function draw() {
   // Get user input
   wavelength = sliderWavelength.value();
   wavelengthBox.value(wavelength);
-  frequency = sliderFrequency.value();
-  frequencyBox.value(frequency);
   amplitude = sliderAmplitude.value();
   amplitudeBox.value(amplitude);
+  timeperiod = sliderTimeperiod.value();
+  timeperiodBox.value(timeperiod);
 
-  // Draw wave
+  // draw moving sine wave with user input of wavelength, frequency, and amplitude
   noFill();
   stroke(0);
   beginShape();
-  for (let x = 0; x <= width; x += 10) {
-    let y = amplitude * sin(2 * PI * (x / wavelength - time_now * frequency) + phase);
-    vertex(x, height / 2 + y);
+  width_sim = width / sim_1m_x;
+  for (let x = 0; x <= width_sim; x += width_sim/500) {
+    y = amplitude * sin(2 * PI * (x / wavelength - time_now / timeperiod) + phase);
+    x_pixels = x * sim_1m_x;
+    y_pixels = y * sim_1m_y;
+    vertex(x_pixels, height / 2 + y_pixels);
   }
-  endShape();
 
+
+  // for (let x = 0; x <= width; x += 0.5*sim_1m_x) {
+  //   y = amplitude * sin(2 * PI * (x / wavelength - time_now / timeperiod) + phase);
+  //   vertex(x, height / 2 + y);
+  // }
+  endShape();
+  
   // Update time
-  time_now += 0.01;
+  time_now += sim_time_step_secs;
 }
 
 function windowResized() {
